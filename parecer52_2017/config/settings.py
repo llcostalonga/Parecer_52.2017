18e911021432a4df082500dd697d0e2d11e6e07f
Leandro Lesqueves Costalonga <llcostalonga@gmail.com>
2021-01-12 10:58:24 -0300

    backup - migrating computers

--- parecer52_2017/config/settings.py
+++ parecer52_2017/config/settings.py
@@ -18,3 +18,7 @@ file_parecer_aceleracao = resources_folder / "parecer_aceleracao.docx"
 file_parecer_anexoIII = resources_folder / "anexoIII.docx"
 
 remover_disciplinas_grad_duplicatas = False
+
+calcular_filtro_periodo = False # Se for falso, usa os valores informados manualmente aqui.
+filtro_periodo_manual = ["2019/3","2019/2", "2019/1", "2018/3"]
+
